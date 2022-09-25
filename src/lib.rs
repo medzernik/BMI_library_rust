a082a3d9d361636b5f8706ec374dd8c541087595
medzernik <medzernik1@gmail.com>
2022-09-25 18:16:01 +0200

    iterator fun

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,44 @@
 extern crate core;
-use chrono::{DateTime, Utc};
+
+use chrono::{Date, DateTime, Utc};
+use std::borrow::{Borrow, BorrowMut};
 use std::ops::Sub;
-use time::PrimitiveDateTime;
 
 //use time::macros::{date, datetime};
 
 pub fn test() {
     let current_time = Utc::now();
 
+    let Utc_test: Date<Utc> = Utc::today();
+
+    println!("Today's date (finding this one): {}", Utc_test);
+
+    let main_template_struct = History::generate_template();
+
+    println!("Struct is:\n {:?}", main_template_struct);
+
+    println!(
+        "Result of search is:\n {:?}",
+        main_template_struct.get_data_specific(Utc_test)
+    );
+
+
+
+    println!("BMI filled:\n {:?}", main_template_struct.clone().get_bmi());
+
+    /*
     println!("{}", Utc::now().format("%d/%m/%Y %H:%M"));
 
     println!("Duration -1 Day {}", get_date(current_time, 1));
     println!("Duration -1 Week {}", get_date(current_time, 2));
     println!("Duration -4 Weeks {}", get_date(current_time, 3));
     println!("Duration -1 Year {}", get_date(current_time, 4));
+
+     */
 }
 
 //get date (calculate date)
-fn get_date(date: DateTime<Utc>, unit: u8) -> DateTime<Utc> {
+fn get_date(date: Date<Utc>, unit: u8) -> Date<Utc> {
     match unit {
         1 => date.sub(chrono::Duration::days(1)),   //day
         2 => date.sub(chrono::Duration::weeks(1)),  //week
@@ -29,17 +50,59 @@ fn get_date(date: DateTime<Utc>, unit: u8) -> DateTime<Utc> {
 //data struct na save dat (output file?)
 //struct: tep, tlak, SpO2, BMI
 
+#[derive(Debug, Clone)]
 struct History {
     entry: Vec<OneMeasurement>,
 }
 
 impl History {
-    fn get_data_specific(self, date_to_find: DateTime<Utc>) {
-        let output: _ = self.entry.iter().filter(|x| x.date == date_to_find).collect();
-        println!("Output of specific date:\n {}", output);
+    fn generate_template() -> History {
+        let output: History = History {
+            entry: vec![
+                OneMeasurement {
+                    heart_rate: 55,
+                    blood_pressure: 120,
+                    sp02: 98,
+                    weight: 58.0,
+                    height: 194.3,
+                    bmi: 0.0,
+                    date: Utc::today(),
+                },
+                OneMeasurement {
+                    heart_rate: 43,
+                    blood_pressure: 150,
+                    sp02: 100,
+                    weight: 150.0,
+                    height: 32.4,
+                    bmi: 0.0,
+                    date: Utc::today().sub(chrono::Duration::weeks(5)),
+                },
+            ],
+        };
+
+        return output;
+    }
+
+    fn get_bmi(self) {
+        self.entry
+            .into_iter()
+            .for_each(|mut x| x.bmi = x.weight / x.height.powf(x.height))
+    }
+
+    fn get_data_specific(&self, date_to_find: Date<Utc>) -> Vec<OneMeasurement> {
+        let output = self.entry
+            .iter()
+            .filter(|x| x.date == date_to_find)
+            .map(|x| x.borrow())
+            .collect();
+
+
+
+        output
     }
 }
 
+#[derive(Debug, Clone)]
 struct OneMeasurement {
     heart_rate: u8,
     blood_pressure: u8,
@@ -47,16 +110,7 @@ struct OneMeasurement {
     weight: f64,
     height: f64,
     bmi: f64,
-    date: DateTime<Utc>,
-}
-
-impl OneMeasurement {
-    fn get_bmi(&mut self) {
-        self.bmi = self.weight / (self.height * self.height);
-    }
-
-
-
+    date: Date<Utc>,
 }
 
 //get bmi (calculate bmi)
