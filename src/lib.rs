e74c99720cc2a57fcfb96880e40a524cfcba2035
medzernik <medzernik1@gmail.com>
2022-09-25 14:29:03 +0200

    fixed dates

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,26 @@ pub fn test() {
     let current_time = Utc::now();
 
     println!("{}", Utc::now().format("%d/%m/%Y %H:%M"));
+
+    println!("Duration -1 Day {}", get_date(current_time, 1));
+    println!("Duration -1 Week {}", get_date(current_time, 2));
+    println!("Duration -4 Weeks {}", get_date(current_time, 3));
+    println!("Duration -1 Year {}", get_date(current_time, 4));
 }
 
 //get date (calculate date)
 fn get_date(date: DateTime<Utc>, unit: u8) -> DateTime<Utc> {
+
+
     match unit {
-        1 => date.sub(time::Duration::days(1)),   //day
-        2 => date.sub(time::Duration::weeks(1)),  //week
-        3 => date.sub(time::Duration::weeks(4)),  //month (4 weeks)
-        4 => date.sub(time::Duration::weeks(52)), //year (52 weeks)
+        1 => date.sub(chrono::Duration::days(1)),   //day
+        2 => date.sub(chrono::Duration::weeks(1)),  //week
+        3 => date.sub(chrono::Duration::weeks(4)),  //month (4 weeks)
+        4 => date.sub(chrono::Duration::weeks(52)), //year (52 weeks)
         _ => panic!("Impossible value"),
     }
+
+
 }
 //data struct na save dat (output file?)
 
