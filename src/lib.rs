519b5be9835f3b7bb299bfccda2f9e9d76dd6321
medzernik <medzernik1@gmail.com>
2022-10-02 20:36:00 +0200

    working on the comparator

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,8 @@ pub fn test() {
 
     println!("Today's date (finding this one): {:?}", utc_test);
 
-
     //generate an empty template
     let mut main_template_struct: History = History::generate_template();
-
 
     //define new 2 entries and append them
     main_template_struct.new_entry(55, 120, 80, 98, 190.0, 75.3, Utc::today());
@@ -42,7 +40,7 @@ pub fn test() {
 
     println!();
 
-    compare_data(&main_template_struct, &utc_test);
+    compare_data(&main_template_struct, 2);
 }
 
 ///get date gets a corresponding date offset.
@@ -56,9 +54,43 @@ fn get_date(date: Date<Utc>, unit: u8) -> Date<Utc> {
     }
 }
 
-fn compare_data(data: &History, check_date: &Date<Utc>) {
-    let last_day_data = data.get_data_specific(&get_date(Utc::today(), 1));
-    println!("{:?}", last_day_data);
+fn compare_data(data: &History, check_date: u8) {
+    let last_day_data = match data.get_data_specific(&get_date(Utc::today(), 1)) {
+        Some(T) => T
+            .first()
+            .map(|x| *x)
+            .expect("Error unwrapping last_day_data"),
+        None => panic!("Error, cannot find data specified"),
+    };
+
+    let chosen_data = match data.get_data_specific(&get_date(Utc::today(), check_date)) {
+        Some(T) => T.first().map(|x| *x).expect("Error unwrapping chosen_data"),
+        None => panic!("Error, cannot find data specified"),
+    };
+
+    //TODO: Make this work... subtract in a normal way.
+    let mut difference: OneMeasurement = OneMeasurement {
+        id: 0,
+        heart_rate: last_day_data.heart_rate- chosen_data.heart_rate,
+        blood_pressure_systolic: last_day_data.blood_pressure_systolic
+            - chosen_data.blood_pressure_systolic,
+        blood_pressure_diastolic: last_day_data.blood_pressure_diastolic
+            - chosen_data.blood_pressure_diastolic,
+        sp_o2: last_day_data.sp_o2 - chosen_data.sp_o2,
+        weight_kilo: last_day_data.weight_kilo - chosen_data.weight_kilo,
+        height_centim: last_day_data.height_centim - chosen_data.height_centim,
+        bmi: 0.0,
+        date: last_day_data.date,
+    };
+
+    difference.bmi = {
+        let intermediate = (difference.weight_kilo / difference.height_centim.powf(2.0)) * 100000.;
+
+        //round to a specified decimal place
+        intermediate.round() / 100.0
+    };
+
+    println!("Last day data: {:#?}", difference);
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -68,8 +100,8 @@ struct OneMeasurement {
     blood_pressure_systolic: u8,
     blood_pressure_diastolic: u8,
     sp_o2: u8,
-    weight_cm: f64,
-    height_kg: f64,
+    weight_kilo: f64,
+    height_centim: f64,
     bmi: f64,
     date: Date<Utc>,
 }
@@ -90,10 +122,10 @@ impl History {
                     blood_pressure_systolic: 120,
                     blood_pressure_diastolic: 80,
                     sp_o2: 98,
-                    weight_cm: 190.0,
-                    height_kg: 75.3,
+                    weight_kilo: 190.0,
+                    height_centim: 75.3,
                     bmi: 0.0,
-                    date: Utc::today().sub(chrono::Duration::weeks(1)),
+                    date: Utc::today(),
                 },
                 OneMeasurement {
                     id: 1,
@@ -101,10 +133,32 @@ impl History {
                     blood_pressure_systolic: 140,
                     blood_pressure_diastolic: 35,
                     sp_o2: 100,
-                    weight_cm: 170.0,
-                    height_kg: 90.7,
+                    weight_kilo: 170.0,
+                    height_centim: 90.7,
                     bmi: 0.0,
-                    date: Utc::today(),
+                    date: Utc::today().sub(chrono::Duration::weeks(1)),
+                },
+                OneMeasurement {
+                    id: 2,
+                    heart_rate: 22,
+                    blood_pressure_systolic: 134,
+                    blood_pressure_diastolic: 44,
+                    sp_o2: 80,
+                    weight_kilo: 175.0,
+                    height_centim: 56.7,
+                    bmi: 0.0,
+                    date: Utc::today().sub(chrono::Duration::weeks(4)),
+                },
+                OneMeasurement {
+                    id: 3,
+                    heart_rate: 85,
+                    blood_pressure_systolic: 140,
+                    blood_pressure_diastolic: 35,
+                    sp_o2: 100,
+                    weight_kilo: 170.0,
+                    height_centim: 90.7,
+                    bmi: 0.0,
+                    date: Utc::today().sub(chrono::Duration::weeks(52)),
                 },
             ],
         }
@@ -153,22 +207,34 @@ impl History {
             blood_pressure_systolic,
             blood_pressure_diastolic,
             sp_o2,
-            weight_cm,
-            height_kg,
+            weight_kilo: weight_cm,
+            height_centim: height_kg,
             bmi: 0.0,
             date,
         });
 
         self.generate_bmi();
     }
-
+    /// Goes through the entire History struct and calculates the BMI for each entry.
+    ///
+    /// Used automatically, when .new_entry is called.
+    ///
+    /// Example:
+    /// ```
+    /// use chrono::{Date, DateTime, Utc};
+    /// let mut main_template_struct: History = History::generate_template();
+    ///
+    /// main_template_struct.get_bmi();
+    ///
+    /// assert_eq!(main_template_struct.entry.bmi, 33.51);
+    /// ```
     fn generate_bmi(&mut self) {
         self.entry.iter_mut().for_each(|x| {
             println!("BMI is now set to: {}: {}", x.id, x.bmi);
 
             //calculate the BMI, prepare to round to a specified decimal place
             //(*1000 for display + *100 for the next calculation)
-            x.bmi = (x.weight_cm / x.height_kg.powf(2.0)) * 100000.;
+            x.bmi = (x.weight_kilo / x.height_centim.powf(2.0)) * 100000.;
 
             //round to a specified decimal place
             x.bmi = x.bmi.round() / 100.0;
